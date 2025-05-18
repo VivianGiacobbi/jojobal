9de780668d428452b1c5b2c79c6fd22801b66afb
Vivi <viviangiacobbi@gmail.com>
2025-05-18 15:25:35 -0500

    fixed reference

--- items/stands/steel_civil.lua
+++ items/stands/steel_civil.lua
@@ -17,7 +17,7 @@ SMODS.Consumable:take_ownership('c_fool', {
         -- imported cardsauce logic
         local last_tarot_planet = localize('k_none')
         if fool_c and fool_c.key == 'c_csau_arrow' then
-            last_tarot_planet = fool_c and localize{type = 'name_text', key = fool_c.key, set = fool_c.set, vars = { G.GAME.csau_max_stands or 1, (card.area.config.collection and localize('k_csau_stand')) or (G.GAME.csau_max_stands > 1 and localize('b_csau_stand_cards') or localize('k_csau_stand')) }} or localize('k_none')
+            last_tarot_planet = fool_c and localize{type = 'name_text', key = fool_c.key, set = fool_c.set, vars = { G.GAME.modifiers.max_stands or 1, (card.area.config.collection and localize('k_csau_stand')) or (G.GAME.modifiers.max_stands > 1 and localize('b_csau_stand_cards') or localize('k_csau_stand')) }} or localize('k_none')
         else
             last_tarot_planet = fool_c and localize{type = 'name_text', key = fool_c.key, set = fool_c.set} or localize('k_none')
         end
