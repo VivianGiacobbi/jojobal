0afcfbce587cc57ca6c092c22f6ab2bdcd887281
Vivi <viviangiacobbi@gmail.com>
2025-12-15 12:59:30 -0800

    Fixed credits

--- main.lua
+++ main.lua
@@ -37,55 +37,57 @@ ArrowAPI.ui.add_badge_colors(SMODS.current_mod, {
 	te_lands = HEX('409CE8'),
 })
 
-ArrowAPI.config_tools.use_credits(JojobalMod, {
-    matrix = {col = 20, row = 16},
-    {
-        key = 'direction',
-        title_colour = G.C.YELLOW,
-        pos_start = {col = 0, row = 0},
-        pos_end = {col = 5, row = 16},
-        contributors = {
-            ["BarrierTrio/Gote"] = true,
-			["Kekulism"] = true,
-            ["Vivian Giacobbi"] = true,
-        }
-    },
-    {
-        key = 'artist',
-        title_colour = G.C.ETERNAL,
-        pos_start = {col = 5, row = 0},
-        pos_end = {col = 12, row = 16}
-    },
-    {
-        key = 'programmer',
-        title_colour = G.C.GOLD,
-        pos_start = {col = 12, row = 0},
-        pos_end = {col = 16, row = 10},
-    },
-    {
-        key = 'shader',
-        title_colour = G.C.DARK_EDITION,
-        pos_start = {col = 16, row = 0},
-        pos_end = {col = 20, row = 10},
-        contributors = {
-            ["Vivian Giacobbi"] = true,
-        }
-    },
-    {
-        key = 'special',
-        title_colour = G.C.GREEN,
-        pos_start = {col = 12, row = 10},
-        pos_end = {col = 20, row = 16},
-        contributors = {
-            ["Hirohiko Araki"] = true,
-            ["LuckyLand Communications"] = true,
-        }
-    },
-})
+if not Cardsauce then
+    -- cardsauce credits also include these
+    ArrowAPI.config_tools.use_credits(JojobalMod, {
+        {
+            key = 'direction',
+            title_colour = G.C.YELLOW,
+            pos_start = {col = 0, row = 0},
+            pos_end = {col = 5, row = 16},
+            contributors = {
+                {name = "BarrierTrio/Gote"},
+                {name = "Kekulism"},
+                {name = "Vivian Giacobbi"},
+            }
+        },
+        {
+            key = 'artist',
+            title_colour = G.C.ETERNAL,
+            pos_start = {col = 5, row = 0},
+            pos_end = {col = 12, row = 16}
+        },
+        {
+            key = 'programmer',
+            title_colour = G.C.GOLD,
+            pos_start = {col = 12, row = 0},
+            pos_end = {col = 16, row = 10},
+        },
+        {
+            key = 'shader',
+            title_colour = G.C.DARK_EDITION,
+            pos_start = {col = 16, row = 0},
+            pos_end = {col = 20, row = 10},
+            contributors = {
+                {name = "Vivian Giacobbi"},
+            }
+        },
+        {
+            key = 'special',
+            title_colour = G.C.GREEN,
+            pos_start = {col = 12, row = 10},
+            pos_end = {col = 20, row = 16},
+            contributors = {
+                {name = "Hirohiko Araki"},
+                {name = "LuckyLand Communications"},
+            }
+        },
+    })
 
-ArrowAPI.config_tools.use_config(JojobalMod, {
-	{key = 'enable_Logo', order = 1, default_value = true},
-})
+    ArrowAPI.config_tools.use_config(JojobalMod, {
+        {key = 'enable_Logo', order = 1, default_value = true},
+    })
+end
 
 ArrowAPI.game.add_game_globals_func(JojobalMod, function(run_start)
     jojobal_reset_paper_rank()
